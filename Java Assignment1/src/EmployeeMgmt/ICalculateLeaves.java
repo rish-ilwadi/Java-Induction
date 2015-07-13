f5f3fed4e567b6b11e822a2de8b86fff2c2b17b2
rishabi-optimus <rishab.ilwadi@optimusinfo.com>
2015-07-13 19:33:10 +0530

    Committed the Assignment Day1 on Java(J2SE).

---
+++ Java Assignment1/src/EmployeeMgmt/ICalculateLeaves.java
@@ -0,0 +1,13 @@
+package EmployeeMgmt;
+
+/*
+ * package EmployeeMgmt
+ * public Interface ICalculateLeaves
+ * 
+ * Provides abstract method countLeaves() for Calculating the leaves
+ */
+
+public interface ICalculateLeaves {
+
+	public int countLeaves(int LeavesRequest);
+}
